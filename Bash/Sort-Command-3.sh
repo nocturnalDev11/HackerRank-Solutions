0901fff201773e3c831598ddbb380272e16b33c2
Lutreze Hue Jacinto <86185117+nocturnalDev11@users.noreply.github.com>
2024-08-12 10:43:34 -0400

    Create Sort-Command-3.sh

---
+++ Bash/Sort-Command-3.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Read input from standard input and sort the numbers numerically
+# The -n flag tells `sort` to treat the input as numbers
+# The numbers are sorted in ascending order
+
+sort -n
